286db52cd31bf61ad24af161d28af347984c1aca
Oksana <deminaon@gmail.com>
2023-06-29 20:49:53 +1000

    Домашнее задание 18.1_2

--- cypress/e2e/santa-box-create.cy.js
+++ cypress/e2e/santa-box-create.cy.js
@@ -15,20 +15,24 @@ describe("user can create a box and run it", () => {
   let currency = "Евро";
   let inviteLink;
 
+  beforeEach(() => {
+    cy.wait(10000); // Ждём 10 секунд перед каждым тестом, чтобы обойти проблему с слишком большим кол-вом запросов к серверу
+  });
+
   it("user logins and create a box", () => {
     cy.visit("/login");
     cy.login(users.userAutor.email, users.userAutor.password);
     cy.contains("Создать коробку").click();
     cy.get(boxPage.boxNameField).type(newBoxName);
-    cy.get(generalElements.arrowRight).click({ force: true });
+    cy.get(generalElements.arrowRight).click();
     cy.get(boxPage.sixthIcon).click();
-    cy.get(generalElements.arrowRight).click({ force: true });
-    cy.get(boxPage.giftPriceToggle).check({ force: true });
+    cy.get(generalElements.arrowRight).click();
+    cy.get(boxPage.giftPriceToggle).check({force: true});
     cy.get(boxPage.maxAnount).type(maxAmount);
     cy.get(boxPage.currency).select(currency);
-    cy.get(generalElements.arrowRight).click({ force: true });
-    cy.get(generalElements.arrowRight).click({ force: true });
-    cy.get(generalElements.arrowRight).click({ force: true });
+    cy.get(generalElements.arrowRight).click();
+    cy.wait(1000); // Необходимо дождаться ответа от сервера
+    cy.get(generalElements.arrowRight).click();
     cy.get(dashboardPage.createdBoxName).should("have.text", newBoxName);
     cy.get(menu.menuBoxes)
       .invoke("text")
@@ -40,7 +44,7 @@ describe("user can create a box and run it", () => {
   });
 
   it("add participants", () => {
-    cy.get(generalElements.submitButton).click();
+    cy.get(generalElements.submitButton).click({force: true});
     cy.get(invitePage.inviteLink)
       .invoke("text")
       .then((link) => {
@@ -56,10 +60,11 @@ describe("user can create a box and run it", () => {
     cy.login(users.user1.email, users.user1.password);
     cy.contains("Создать карточку участника").should("exist");
     cy.get(generalElements.submitButton).click();
-    cy.get(generalElements.arrowRight).click({ force: true });
-    cy.get(generalElements.arrowRight).click({ force: true });
+    cy.get(generalElements.arrowRight).click({force: true});
+    cy.wait(1000); // Необходимо дождаться ответа от сервера
+    cy.get(generalElements.arrowRight).click({force: true});
     cy.get(inviteeBoxPage.wishesInput).type(wishes);
-    cy.get(generalElements.arrowRight).click({ force: true });
+    cy.get(generalElements.arrowRight).click({force: true});
     cy.get(inviteeDashboardPage.noticeForInvitee)
       .invoke("text")
       .then((text) => {
@@ -70,15 +75,16 @@ describe("user can create a box and run it", () => {
 
   it("approve as user2", () => {
     cy.visit(inviteLink);
-    cy.get(generalElements.submitButton).click();
+    cy.get(generalElements.submitButton).click({ force: true });
     cy.contains("войдите").click();
     cy.login(users.user2.email, users.user2.password);
     cy.contains("Создать карточку участника").should("exist");
     cy.get(generalElements.submitButton).click();
-    cy.get(generalElements.arrowRight).click({ force: true });
-    cy.get(generalElements.arrowRight).click({ force: true });
+    cy.get(generalElements.arrowRight).click({force: true});
+    cy.wait(1000); // Необходимо дождаться ответа от сервера
+    cy.get(generalElements.arrowRight).click({force: true});
     cy.get(inviteeBoxPage.wishesInput).type(wishes);
-    cy.get(generalElements.arrowRight).click({ force: true });
+    cy.get(generalElements.arrowRight).click({force: true});
     cy.get(inviteeDashboardPage.noticeForInvitee)
       .invoke("text")
       .then((text) => {
@@ -88,17 +94,16 @@ describe("user can create a box and run it", () => {
   });
 
   it("approve as user3", () => {
-    cy.wait(3000);
     cy.visit(inviteLink);
     cy.get(generalElements.submitButton).click();
     cy.contains("войдите").click();
     cy.login(users.user3.email, users.user3.password);
     cy.contains("Создать карточку участника").should("exist");
     cy.get(generalElements.submitButton).click();
-    cy.get(generalElements.arrowRight).click({ force: true });
-    cy.get(generalElements.arrowRight).click({ force: true });
+    cy.get(generalElements.arrowRight).click({force: true});
+    cy.get(generalElements.arrowRight).click({force: true});
     cy.get(inviteeBoxPage.wishesInput).type(wishes);
-    cy.get(generalElements.arrowRight).click({ force: true });
+    cy.get(generalElements.arrowRight).click({force: true});
     cy.get(inviteeDashboardPage.noticeForInvitee)
       .invoke("text")
       .then((text) => {
@@ -107,20 +112,30 @@ describe("user can create a box and run it", () => {
     cy.clearCookies();
   });
 
+  it("toss", () => {
+    cy.visit("/login");
+    cy.login(users.userAutor.email, users.userAutor.password);
+    cy.contains("Коробки").click({ force: true });
+    cy.get(boxPage.box).last().click({ force: true });
+    cy.contains("Перейти к жеребьевке").click({ force: true });
+    cy.contains("Провести жеребьевку").click({ force: true });
+    cy.contains("Да, провести жеребьевку").click({ force: true });
+    cy.contains("Жеребьевка проведена").should("exist");
+    cy.clearCookies();
+  });
+
   after("delete box", () => {
     cy.visit("/login");
     cy.login(users.userAutor.email, users.userAutor.password);
-    cy.get(
-      '.layout-1__header-wrapper-fixed > .layout-1__header > .header > .header__items > .layout-row-start > [href="/account/boxes"] > .header-item > .header-item__text > .txt--med'
-    ).click();
-    cy.get(":nth-child(1) > a.base--clickable > .user-card").first().click({ force: true });
+    cy.get('.layout-1__header-wrapper-fixed > .layout-1__header > .header > .header__items > .layout-row-start > [href="/account/boxes"] > .header-item > .header-item__text > .txt--med').click({force: true});
+    cy.get(boxPage.box).first().click({force: true});
     cy.get(
       ".layout-1__header-wrapper-fixed > .layout-1__header-secondary > .header-secondary > .header-secondary__right-item > .toggle-menu-wrapper > .toggle-menu-button > .toggle-menu-button--inner"
-    ).click();
-    cy.contains("Архивация и удаление").click({ force: true });
+    ).click({force: true});
+    cy.contains("Архивация и удаление").click({force: true});
     cy.get(":nth-child(2) > .form-page-group__main > .frm-wrapper > .frm").type(
       "Удалить коробку"
     );
-    cy.get(".btn-service").click({ force: true });
+    cy.get(".btn-service").click({force: true});
   });
 });
