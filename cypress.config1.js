bc39a59e5228925acb556e8ded6451815eb6727d
Oksana <deminaon@gmail.com>
2023-07-11 16:31:13 +1000

    Домашнее задание 21.1(ч.2)

---
+++ cypress.config1.js
@@ -0,0 +1,28 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+
+
+module.exports = defineConfig({
+  e2e: {
+    projectId: 'as4p1t',
+    video: false,
+    viewportWidth: 1280,
+    viewportHeight: 800,
+    baseUrl: "https://santa-secret.ru/",
+    testIsolation: false,
+    specPattern: "**/*.feature",
+    setupNodeEvents(on, config) {
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+        
+        on("file:preprocessor", bundler);
+        addCucumberPreprocessorPlugin(on, config);
+        
+        return config;
+    },
+
+  },
+});
--- cypress.config2.js
+++ cypress.config2.js
@@ -8,6 +8,8 @@ module.exports = defineConfig({
   e2e: {
     projectId: 'as4p1t',
     video: false,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     baseUrl: "https://santa-secret.ru/",
     testIsolation: false,
     specPattern: "**/*.feature",
