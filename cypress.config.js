9b59a9f2954253e00d5e18f4a130db7dbcbaf8e0
Oksana <deminaon@gmail.com>
2023-07-11 23:01:10 +1000

    Домашнее задание 22.1

---
+++ cypress.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+
+
+module.exports = defineConfig({
+  e2e: {
+    projectId: 'as4p1t',
+    video: false,
+    baseUrl: "https://santa-secret.ru/",
+    testIsolation: false,
+    specPattern: "**/*.feature",
+    setupNodeEvents(on, config) {
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+        
+        on("file:preprocessor", bundler);
+        addCucumberPreprocessorPlugin(on, config);
+        
+        return config;
+    },
+
+  },
+});
